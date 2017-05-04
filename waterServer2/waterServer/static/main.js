af05eef9899f7229b1e4c324b1560b3d0440439b
Otto Vogrincic <ovogrin@gmail.com>
2017-05-04 10:26:53 -0400

    Aqua Pi Server Code

---
+++ waterServer2/waterServer/static/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	var number;
+	$('#alertBtn').click(function(){
+		alert("Pumping Water!!!");
+	});
+
+	$(function(){
+	   $(".clickable").click(function(){
+		$(this).addClass("active");
+	   });
+	});
+
+});
---
+++ waterServer2/waterServer/water.py
@@ -0,0 +1,15 @@
+import flask, flask.views
+import os
+import utils
+
+class Water(flask.views.MethodView):
+	@utils.login_required
+	def get(self):
+		return flask.render_template('water.html')
+
+	@utils.login_required
+	def post(self):
+		result = os.system("python /home/pi/waterServer/startWaterPump.py")
+#		result = eval(flask.request.form['expression'])
+#		flask.flash(result)
+		return flask.redirect(flask.url_for('water'))
